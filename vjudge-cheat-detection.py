a74d2ca29879bcfe01f2b3ba044406242718d590
ned_chu <243502796@qq.com>
2018-10-13 22:04:47 +0800

    feat: very big patch.
    1. add good support to Moss's command line parameters.
    2. support YAML config file.
    3. keep one copy of a user when specified.
    4. preprocess of submission code: ignore problem, ignore user, rename *.c to *.cpp
    5. fix some encoding problem.

--- vjudge-cheat-detection.py
+++ vjudge-cheat-detection.py
@@ -9,12 +9,22 @@ import difflib
 import mosspy
 import glob
 import collections
+import logging
+import mosspy
+import yaml
+
 from jinja2 import Template, PackageLoader, Environment
 
 
-SIMILAR_RATIO_BOUND = 0.9
+def ensure_list(item):
+    if item is None:
+        return []
+    elif type(item) is list:
+        return item
+    else:
+        return [item]
 
-def get_args():
+def get_config():
     parser = argparse.ArgumentParser(description='Detect cheat in vjudge contest.')
 
     parser.add_argument(dest='files', type=str, nargs='+',
@@ -23,11 +33,27 @@ def get_args():
                         action='store_true', help='build zip archive for output')
     parser.add_argument('-u', '--unique', dest='unique_submission', action='store_true',
                         help='retain only one copy of code when meeting similar submissions of a single user')
-    parser.add_argument('-m', '--moss', dest='moss_userid', default=None, nargs=1, help='specify moss userid and use moss to detect cheating, regiester one if you don\'t have your own userid')
+    parser.add_argument('-m', '--moss', dest='moss_userid', action='store', help='specify moss userid and use moss to detect cheating, regiester one if you don\'t have your own userid')
+    parser.add_argument('-c', '--config', dest='config_file', action='store', help='config file')
+    parser.add_argument('-iu', '--ignore-userid', nargs='+', dest='ignore_userid', default=[])
+    parser.add_argument('-ip', '--ignore-problem', nargs='+', dest='ignore_problem', default=[])
+    parser.add_argument('-l', '--language', dest='language', nargs='+', default=['cc'])
+    parser.add_argument('-b', '--base-dir', dest='base_dir')
+    parser.add_argument('-M', '--maximal-match', type=int, dest='maximal_match', default=10)
+    parser.add_argument('-n', '--report-number', type=int, dest='report_number', default=250)
 
-    return parser.parse_args()
+    config = parser.parse_args()
+    if config.config_file is not None:
+        with open(config.config_file, "r", encoding='utf-8') as f:
+            y = yaml.load(f)
+            config.moss_userid = config.moss_userid or y.get("moss_userid")
+            config.output_zip = config.output_zip or y.get("output_zip")
+            config.unique_submission = config.unique_submission or y.get("unique_submission")
+            config.ignore_userid.extend(ensure_list(y.get("ignore_userid", [])))
+            config.language.extend(ensure_list(y.get("language", [])))
+    return config
 
-
+# prepare
 def unzip_file(zip_file_name, unzip_dir):
     try:
         with zipfile.ZipFile(zip_file_name) as zfile:
@@ -48,6 +74,14 @@ def get_contest_title(contest_id):
         return json.loads(json_html.text)["title"] or None
 
 
+# preprocess
+def output_list(comment, item_list):
+    if len(item_list) == 0:
+        return
+    print(comment)
+    print('\n'.join(item_list))
+
+
 def remove_duplicate(submission_dir):
     user_sub = {}
     for sub in os.listdir(submission_dir):
@@ -55,31 +89,55 @@ def remove_duplicate(submission_dir):
         user_sub.setdefault(user_id, []).append(os.path.join(submission_dir, sub))
     
     remove_list = []
-    for submissions in user_sub.values():
-        l = len(submissions)
-        f = [open(sub, 'r') for sub in submissions]
-        s = list(map(lambda x: ''.join(x.readlines()), f))
-        s = list(map(lambda x: ' '.join(x.split()), s))
-        for i in range(l):
-            should_remove = False
-            for j in range(i):
-                sim = difflib.SequenceMatcher(None, s[i], s[j]).ratio()
-                should_remove |= sim >= SIMILAR_RATIO_BOUND
-            if should_remove:
-                remove_list.append(submissions[i])
-        
-        for ff in f:
-            ff.close()
+    for user_id in user_sub:
+        remove_list.extend(sorted(user_sub[user_id])[:-1])
     
     for file_name in remove_list:
         os.remove(file_name)
     return remove_list
 
+def preprocess(unzip_dir, unique_submission, ignore_problem, ignore_userid):
+    all_removed = []
+    for problem in ignore_problem:
+        problem_dir = os.path.join(unzip_dir, problem)
+        if os.path.exists(problem_dir):
+            shutil.rmtree(problem_dir)
+            all_removed.append(problem_dir)
+    output_list("Ignored problem:", all_removed)
 
+    all_removed = []
+    for problem in os.listdir(unzip_dir):
+        problem_dir = os.path.join(unzip_dir, problem)
+        for sub in os.listdir(problem_dir):
+            user_id = '_'.join(sub.split('_')[1:-1])
+            if user_id in ignore_userid:
+                all_removed.append(os.path.join(problem_dir, sub))
+    for f in all_removed:
+        os.remove(f)
+    output_list("Ignore submission by userid:", all_removed)
+
+    if config.unique_submission:
+        all_removed = []
+        for problem in os.listdir(unzip_dir):
+            submission_dir = os.path.join(unzip_dir, problem)
+            all_removed.extend(remove_duplicate(submission_dir))
+        output_list("Removed duplicate file:", all_removed)
+    
+    # rename *.c to *.cpp
+    rename_list = glob.glob(os.path.join(unzip_dir, '*', '*.c'))
+    for f in rename_list:
+        new_file = f + "pp"
+        if os.path.exists(new_file):
+            os.remove(f)
+        else:
+            os.rename(f, new_file)
+    output_list("Rename c to cpp:", rename_list)
+
+# postprocess
 def replace_html(html_file, source, target):
-    with open(html_file, 'r') as f:
+    with open(html_file, 'r', encoding='GB2312') as f:
         html = f.readlines()
-    with open(html_file, 'w') as f:
+    with open(html_file, 'w', encoding='utf-8') as f:
         f.writelines([line.replace(source, target) for line in html])
     
 
@@ -110,11 +168,14 @@ def build_html(base_dir, all_index_name, build_dir_list):
         f.write(all_index_html)
 
 
-def moss_download(unzip_dir, result_buffer_dir, moss_userid):
+def moss_download(unzip_dir, result_buffer_dir, config):
     language_list = ['cc', 'java', 'python']
     buffer_dir_list = ['c++', 'java', 'python']
     prefix_list = ['cpp', 'java', 'py']
     for language, dir_lan, prefix in zip(language_list, buffer_dir_list, prefix_list):
+        if language not in config.language:
+            continue
+
         for problem in os.listdir(unzip_dir):
             submission_dir = os.path.join(unzip_dir, problem)
             buffer_dir = os.path.join(result_buffer_dir, dir_lan, problem)
@@ -122,15 +183,21 @@ def moss_download(unzip_dir, result_buffer_dir, moss_userid):
 
             if len(list(glob.glob(wildcard))) <= 1:
                 continue
-            print(moss_userid, language, wildcard, list(glob.glob(wildcard)))
-            m = mosspy.Moss(moss_userid, language)
+            m = mosspy.Moss(config.moss_userid, language)
+            m.setIgnoreLimit(config.maximal_match)
+            m.setNumberOfMatchingFiles(config.report_number)
+            if config.base_dir is not None:
+                moss_base_file = os.path.join(config.base_dir, f"{problem}.cpp")
+                if os.path.exists(moss_base_file):
+                    m.addBaseFile(moss_base_file, "base")
+            wildcard = os.path.join(submission_dir, f"*.{prefix}")
             m.addFilesByWildcard(wildcard)
             url = m.send()
 
             print("Report Url: " + url)
             if not url.startswith("http"):
                 continue
-            mosspy.download_report(url, buffer_dir + os.sep, connections=8)
+            mosspy.download_report(url, buffer_dir + os.sep, 8, logging.INFO)
 
 
 def moss_build(contest_title, base_dir, result_dir):
@@ -147,9 +214,12 @@ def moss_build(contest_title, base_dir, result_dir):
         build_html(base_dir, f'{contest_title} {dir_lan}.html', build_dir_list)
 
 
-def process(file, output_zip=False, unique_submission=False, moss_userid=None):
+def process(file, config):
     if not os.path.exists(file):
         print(f"{file} does not exist.")
+        return
+    if config.moss_userid is None:
+        print("Moss userid is needed.")
         return
 
     contest_id = os.path.basename(file[:-4])
@@ -157,33 +227,31 @@ def process(file, output_zip=False, unique_submission=False, moss_userid=None):
 
     base_dir = os.path.join(os.path.dirname(file), contest_title)
     unzip_dir = os.path.join(base_dir, "submissions")
+    if os.path.exists(unzip_dir):
+        shutil.rmtree(unzip_dir)
     if not unzip_file(file, unzip_dir):
         return
 
-    if unique_submission:
-        all_removed = ["Removed file:"]
-        for problem in os.listdir(unzip_dir):
-            submission_dir = os.path.join(unzip_dir, problem)
-            all_removed.extend(remove_duplicate(submission_dir))
-        print('\n'.join(all_removed) + '\n')
+    preprocess(unzip_dir, config.unique_submission, config.ignore_problem, config.ignore_userid)
     
-    if moss_userid is not None:
-        result_buffer_dir = os.path.join(base_dir, 'result_buffer')
-        result_dir = os.path.join(base_dir, 'result')
-        if not os.path.exists(result_buffer_dir):
-            os.makedirs(result_buffer_dir)
-            moss_download(unzip_dir, result_buffer_dir, moss_userid)
+    result_buffer_dir = os.path.join(base_dir, 'result_buffer')
+    result_dir = os.path.join(base_dir, 'result')
+    if not os.path.exists(result_buffer_dir):
+        os.makedirs(result_buffer_dir)
+    moss_download(unzip_dir, result_buffer_dir, config)
 
-        if os.path.exists(result_dir):
-            shutil.rmtree(result_dir)
-        shutil.copytree(result_buffer_dir, result_dir)
-        moss_build(contest_title, base_dir, result_dir)
+    # copy result buffer and postprocess
+    if os.path.exists(result_dir):
+        shutil.rmtree(result_dir)
+    shutil.copytree(result_buffer_dir, result_dir)
+    moss_build(contest_title, base_dir, result_dir)
 
-    if output_zip:
+    if config.output_zip:
         shutil.make_archive(f"{base_dir} report", 'zip', base_dir)
 
 
 if __name__ == '__main__':
-    args = get_args()
-    for file in args.files:
-        process(file, args.output_zip, args.unique_submission, args.moss_userid[0])
+    config = get_config()
+    print(config)
+    for file in config.files:
+        process(file, config)
